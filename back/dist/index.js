e183cfa29875153be79ba80c718edf3e9b31a06d
Ianta <ianricar4@gmail.com>
2024-07-23 16:09:00 -0300

    primer homework

---
+++ back/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const num1 = 2;
+const num2 = 2;
+const suma = (a, b) => {
+    return a + b;
+};
+console.log(suma(num1, num2));
---
+++ back/src/index.ts
@@ -0,0 +1,7 @@
+const num1:number = 2;
+const num2:number = 2;
+
+const suma=( a:number , b:number ):number => {
+    return a + b;
+}
+console.log(suma(num1,num2));
