77987a8998de3605340b8732c0f250c49202f500
Ian Tarquini <ian@continentalvc.com>
2025-07-29 10:50:13 -0300

    added db

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -3,19 +3,21 @@ import { DataSource } from 'typeorm';
 import { User } from '../entities/User';
 import { Credential } from '../entities/Credential';
 import { Appointment } from '../entities/Appointment';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: 'postgres', 
-    host: 'localhost',
-    port: 5432, 
-    username: 'postgres',
-    password: 'A1b2.@c?',
-    database: 'gimnasio_db',  
-    dropSchema:false,                                    
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dropSchema: false,
     synchronize: true,
     logging: false,
-    entities: [User,Credential,Appointment],
+    entities: [User, Credential, Appointment],
     migrations: [],
     subscribers: [],
 });
